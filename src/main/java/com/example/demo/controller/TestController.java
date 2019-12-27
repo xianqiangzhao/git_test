bec89d7732e2c4d9081d5aa84fbec3641b838e83
yinxinchen <yin.xinchen@trans-cosmos.com.cn>
2019-12-27 10:39:56 +0900

    第一次提交

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public Map<String,String> test(){
+        return Map.of("test","kongsan");
+    }
+    
+}
